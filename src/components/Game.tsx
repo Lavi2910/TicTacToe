379252fd0b5c2cebcad4f12fc465cb38f6f78cd1
sltan29 <almog.sultan29@gmail.com>
2025-12-19 22:46:57 +0200

    moved the winner combinations outside the component, checking if a square is occupied

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,60 +1,63 @@
 import { useEffect, useState } from "react";
 import Board from "./Board";
 
+const winnerCombination = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [6, 4, 2],
+        [0, 3, 6],
+        [2, 5, 8],
+        [1, 4, 7]
+    ]
+
 export default function Game() {
     const [squares, setSquares] = useState<(string | null)[]>(
         Array(9).fill(null)
     );
     const [time, setTime] = useState(10);
     const [isXNext, setIsXNext] = useState(true);
-    const winnerCombination = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,4,8],
-        [6,4,2],
-        [0,3,6],
-        [2,5,8],
-        [1,4,7]
-    ]
+    
 
-    useEffect(()=>{
-        if (time === 0){
-            setIsXNext((prev)=>!prev);
+    useEffect(() => {
+        if (time === 0) {
+            setIsXNext((prev) => !prev);
             setTime(10);
             return;
         }
 
-        const timer = setInterval(()=>{
-            setTime((prev)=>prev - 1)
-        },1000)
+        const timer = setInterval(() => {
+            setTime((prev) => prev - 1)
+        }, 1000)
 
         return () => clearInterval(timer);
-        
-    },[time,squares])
+
+    }, [time, squares])
 
 
     function handleSquareClick(index: number) {
-        if(calculateWinner())
+        if (calculateWinner() || squares[index] != null)
             return;
 
-        squares[index] = isXNext ? "X" : "O";
-        setSquares([...squares]);
+        const nextSquares = [...squares];
+        nextSquares[index] = isXNext ? "X" : "O";
+        setSquares(nextSquares);
         setIsXNext(!isXNext);
         setTime(10);
-        
+
     }
 
-    function calculateWinner(){
-        for (let i = 0; i < winnerCombination.length; i++){
-            const [a,b,c] = winnerCombination[i];
+    function calculateWinner() {
+        for (let i = 0; i < winnerCombination.length; i++) {
+            const [a, b, c] = winnerCombination[i];
             if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
                 return squares[a];
         }
         return null;
     }
 
-    function playAgain(){
+    function playAgain() {
         setSquares(Array(9).fill(null));
         setIsXNext(true);
         setTime(10);
